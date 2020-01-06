6800bcdeb0015ccbc8d62c5e0e7cad126e6477d5
aladinbd <md.alauddin19@gmail.com>
2020-01-06 00:22:20 -0800

    initial commit

---
+++ ex10.py
@@ -0,0 +1,14 @@
+tabby_cat = "\tI'm tabbed in."
+paersian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat Food
+\t* Fishes
+\t* Catnip\n\t* Grass \n\t* Another item \n\t* Another new item
+"""
+
+print(tabby_cat)
+print(paersian_cat)
+print(fat_cat)
