e645f4191fada6e7fc0cde70a632fe4f18c5e276
aladinbd <md.alauddin19@gmail.com>
2020-01-05 21:53:29 -0800

    first commit

---
+++ ex7.py
@@ -0,0 +1,20 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 10) # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "B"
+end7 = "u"
+end8 = "r"
+end9 = "g"
+end10 = "e"
+end11 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end9 + end10 + end11)
